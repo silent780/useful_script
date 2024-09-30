6e33d4e1a9f8eb7259597c977a6f4d2fb1b1594b
silent780 <18095542g@connect.polyu.hk>
2024-09-30 18:16:17 +0800

    update standard error

--- Kmeans_practice.py
+++ Kmeans_practice.py
@@ -11,6 +11,26 @@
 import pandas as pd
 
 
+class Sample_point:
+    """
+    sample point
+    """
+
+    def __init__(self, k_center: None | pd.DataFrame = None, **kwargs) -> None:
+        self.k_center = None
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+    def compute_distance(self, sample_data):
+        """compute the distance between sample data and k center"""
+
+
 class Kmeans:
     """build a simple kmeans algorithm
     input: data: pd.DataFrame
@@ -51,7 +71,7 @@ class Kmeans:
         self.data = pd.DataFrame(Data)
         self.k = K_value
         self.Max_iterations = Max_iterations
-        self.k_center = self.init_k_center(Data, K_value)
+        self.k_center = self.init_k_center()
 
     def filter(self, Threshold):
         """
@@ -61,15 +81,17 @@ class Kmeans:
         pass
 
     def normalize(self):
-        pass
+        """normalize data"""
+        self.data = (self.data - self.data.mean()) / self.data.std()
+        print(self.data.head())
 
-    def init_k_center(self, K_value):
+    def init_k_center(self):
         """init k center
         random choose k center
         if distance between center and data is smaller than threshold,
         then add data to the center
         """
-        k_center = self.data.sample(n=K_value)
+        k_center = self.data.sample(n=self.k)
 
         return k_center
 
@@ -111,9 +133,41 @@ class Kmeans:
         pass
 
 
+def build_example(sample_num: int = 10000):
+    import torch
+    import matplotlib.pyplot as plt
+
+    n_data = torch.ones(sample_num, 2)
+    xy0 = torch.normal(
+        2 * n_data, 1
+    )  # 生成均值为2，2 标准差为1的随机数组成的矩阵 shape=(sample_num, 2)
+    c0 = torch.zeros(sample_num)
+    xy1 = torch.normal(
+        -2 * n_data, 1
+    )  # 生成均值为-2，-2 标准差为1的随机数组成的矩阵 shape=(sample_num, 2)
+    c1 = torch.ones(sample_num)
+    X = torch.cat((xy0, xy1), 0)
+    c = torch.cat((c0, c1), 0)
+
+    plt.scatter(
+        X[:, 0],
+        X[:, 1],
+        c=c,
+        s=10,
+        cmap="RdYlGn",
+        alpha=0.7,
+    )  # 在这个 plt.scatter 函数中，cmap 参数指定了颜色渐变的模式。 "RdYlGn" 是一个预定义的 colormap 名称，表示从红色（Red）到黄色（Yellow）到绿色（Green）的渐变。
+
+    plt.xlabel("x")
+    plt.show()
+    print(X.shape)
+    return X
+
+
 if __name__ == "__main__":
-    data = pd.DataFrame([[1, 2], [3, 4], [5, 6], [7, 8]])
+    data = build_example(100)
+    # data = pd.DataFrame([[1, 2], [3, 4], [5, 6], [7, 8]])
+
     kmeans = Kmeans(data, 2, 100)
-    kmeans.fit()
-    kmeans.report()
-    kmeans.auto_keamns()
+    kmeans.normalize()
+    print(kmeans.init_k_center())
